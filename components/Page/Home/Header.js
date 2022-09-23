ec5000eb7d9a9f4665d2647a69ddf3f8bf24f59a
Rama Sanjaya <ramasanjaya2202@gmail.com>
2022-09-23 07:54:42 +0700

    adding feature and faq

--- components/Page/Home/Header.js
+++ components/Page/Home/Header.js
@@ -1,10 +1,72 @@
 import React from "react";
 import Image from "next/image";
 import Box from "@mui/material/Box";
-
+import {
+  AppBar,
+  Button,
+  Container,
+  MenuItem,
+  Select,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import Grid from "@mui/material/Grid";
+import BorderedBottomBox from "../../Wrapper/BorderedButtomBox";
 const Header = () => {
   return (
-    <Box>
+    <BorderedBottomBox>
+      <AppBar
+        sx={{
+          backgroundColor: "transparent",
+          padding: "25px 20px",
+          position: 'absolute'
+        }}
+        elevation={0}
+      >
+        <Toolbar>
+          <Grid container alignItems="center" spacing={2}>
+            <Grid item xs>
+              <Image
+                src="/__images/netflix.svg"
+                height="45px"
+                width="167px"
+                layout="intrinsic"
+                alt="Netflix logo"
+              />
+            </Grid>
+            <Grid item xs="auto">
+              <Select
+                name="lang"
+                variant="outlined"
+                size="small"
+                defaultValue="EN"
+                sx={{
+                  borderWidth: "2px",
+                  borderStyle: "solid",
+                  borderColor: "common.white",
+                  color: "common.white",
+                  "& .MuiSelect-icon": {
+                    color: "common.white",
+                  },
+                  "& .MuiOutlinedInput-notchedOutline": {
+                    border: "none",
+                  },
+                }}
+              >
+                <MenuItem value="ID">Bahasa Indonesia</MenuItem>
+                <MenuItem value="EN">English</MenuItem>
+              </Select>
+            </Grid>
+            <Grid item xs="auto">
+              <Button color="primary" variant="contained">
+                Sign in
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+
       <Box
         sx={{
           position: "relative",
@@ -36,8 +98,58 @@ const Header = () => {
           objectFit="cover"
           alt="Backdrop Netflix"
         />
+        <Container maxWidth="md" sx={{ position: "relative", zIndex: 1000 }}>
+          <Typography
+            variant="h2"
+            component="h1"
+            color="common.white"
+            fontWeight="600"
+            textAlign="center"
+          >
+            Unlimited movies, TV shows, and more.
+          </Typography>
+          <Typography
+            variant="h5"
+            component="p"
+            color="common.white"
+            textAlign="center"
+            gutterBottom
+          >
+            Watch anywhere. Cancel anytime
+          </Typography>
+          <Typography
+            variant="h6"
+            component="p"
+            color="common.white"
+            textAlign="center"
+            sx={{ my: 3 }}
+          >
+            Ready to watch? Enter your email to create or restart your
+            membership
+          </Typography>
+          <Grid container>
+            <Grid item xs>
+              <TextField
+                variant="filled"
+                label="Email address"
+                fullWidth
+                sx={{ bgcolor: "common.white" }}
+              />
+            </Grid>
+            <Grid item xs="auto">
+              <Button
+                variant="contained"
+                size="large"
+                color="primary"
+                sx={{ height: "100%", borderRadius: "2px" }}
+              >
+                Get Started
+              </Button>
+            </Grid>
+          </Grid>
+        </Container>
       </Box>
-    </Box>
+    </BorderedBottomBox>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Header from '../components/Page/Home/Header'
-
+import Features from '../components/Page/Home/Features'
+import FAQ from '../components/Page/Home/FAQ'
 export default function Home() {
   return (
     <>
     <Header />
+    <Features />
+    <FAQ />
     </>
   );
 }
